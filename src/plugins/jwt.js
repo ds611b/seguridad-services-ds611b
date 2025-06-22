795dacb0e787d5522240790fcf1de9899ed2bbb0
ds611b <dajoislu@gmail.com>
2025-06-22 01:09:58 -0600

    feat: cambio por variable de entorno para refresh del token

--- src/plugins/jwt.js
+++ src/plugins/jwt.js
@@ -5,7 +5,7 @@ import fastifyJwt from '@fastify/jwt'
 export default fp(async (fastify, opts) => {
   fastify.register(fastifyJwt, {
     secret: process.env.JWT_SECRET,
-    sign: { expiresIn: '15m' }
+    sign: { expiresIn: process.env.JWT_REFRESH_EXPIRES }
   })
 
   fastify.decorate('authenticate', async (request, reply) => {
