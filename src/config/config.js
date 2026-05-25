3520d381f6bc6c974f7e40eb16a13f1ec5b85286
ds611b <dajoislu@gmail.com>
2026-05-25 00:18:57 -0600

    fix: update default host to '0.0.0.0' in config

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   port: Number(process.env.PORT) || 3000,
-  host: process.env.HOST || 'localhost',
+  host: process.env.HOST || '0.0.0.0',  
   nodeEnv: process.env.NODE_ENV || 'development',
   docsPath: process.env.DOCS_PATH,
 };
